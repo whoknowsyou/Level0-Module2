1eeea4bcdf8d87a6fa9060d93e7b09788f7c5de0
HeyItzGinger <tumtumjmh@gmail.com>
2017-09-12 18:10:06 -0700

    bananaquiz secrectmessage candycrush

--- src/conditionals/BananaQuiz.java
+++ src/conditionals/BananaQuiz.java
@@ -10,16 +10,31 @@ public class BananaQuiz
 	
 	public static void main(String[] args)
 	{
+		String name = JOptionPane.showInputDialog(null, "what is your name");		
 		//1. ask the user if they like bananas
-		//2. if they say no, 
+		String banana = JOptionPane.showInputDialog(null, "do you like bananas");
+		//2. if they say no,
+		if (banana.equals("no")) {
+			JOptionPane.showMessageDialog(null, "you are crazy");			
+		}
 			//tell them they are crazy 
 			//and end quiz
+		
 		//3. if they say yes
+		if (banana.equals("yes")) {
+			String hobby = JOptionPane.showInputDialog(null, "what is your favorit hobbie");			
+		if (hobby.equals("legos" + "reading")) {
+			JOptionPane.showMessageDialog(null, hobby + " is much better than bananas");			
+		}else{
+	
+
+JOptionPane.showMessageDialog(null, name + " you are bananas!");
+}		}
 		//	ask them what is their favorite hobby
 		//	show a pop up that says "<your hobby> is much better with bananas!"
 
-		//4. OPTIONAL: if they say something other than “yes” or “no”
-		//	show a pop up that says “You are bananas!”
+		//4. OPTIONAL: if they say something other than â€œyesâ€� or â€œnoâ€�
+		//	show a pop up that says â€œYou are bananas!â€�
 	
 	}
 
--- src/conditionals/SecretMessageBox.java
+++ src/conditionals/SecretMessageBox.java
@@ -17,14 +17,26 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
-	
+	public static void main(String[] args) {
+		
+		String make = JOptionPane.showInputDialog(null, "enter password");
 	// 1. Set the passcode in a String variable
-	
-	// 2. Using a pop-up, ask for a secret message and store it in a variable
-
+			// 2. Using a pop-up, ask for a secret message and store it in a variable
+			String mess="";
 	// 3. Ask your friend for the passcode and store it in a variable
-
+if (make.equals("me")) {
+	mess = JOptionPane.showInputDialog(null, "enter message");
+}else{
+	JOptionPane.showMessageDialog(null, "INTRUDER!!");	
+	String password = JOptionPane.showInputDialog(null, "enter password");	
 	// 4. If the passcode matches, show the secret message
+if (password.equals("password")) {
+	JOptionPane.showMessageDialog(null, mess );	
+}else{
+	JOptionPane.showMessageDialog(null, "INTRUDER!!");	
+	}
+}
+}
 
 	// 5. If the passcode does not match, pop-up "INTRUDER!!"
 
